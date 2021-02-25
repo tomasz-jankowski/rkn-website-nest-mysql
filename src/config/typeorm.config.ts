07c708bbb33ee64d331b36f0d082222585b5e220
tomasz-jankowski <tomasz.ad.jankowski@gmail.com>
2021-02-25 20:50:47 +0100

    Update typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,11 +5,11 @@ const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  host: process.env.RDS_HOSTNAME || dbConfig.host,
-  port: process.env.RDS_PORT || dbConfig.port,
-  username: process.env.RDS_USERNAME || dbConfig.username,
-  password: process.env.RDS_PASSWORD || dbConfig.password,
-  database: process.env.RDS_DB_NAME || dbConfig.database,
+  host: process.env.HOSTNAME || dbConfig.host,
+  port: process.env.PORT || dbConfig.port,
+  username: process.env.USERNAME || dbConfig.username,
+  password: process.env.PASSWORD || dbConfig.password,
+  database: process.env.DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
 };
