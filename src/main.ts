3a1637e46ad7bb99405f0ec312a760e031bbe07c
tomasz-jankowski <tomasz.ad.jankowski@gmail.com>
2021-02-25 21:21:32 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(parseInt(process.env.PORT) || 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
